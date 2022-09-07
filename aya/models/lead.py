98ddc320cb6725991953c1389fb2b4e712c96bfd
florvhee <florvhee@gmail.com>
2022-09-07 23:10:37 +0200

    debug

--- aya/models/lead.py
+++ aya/models/lead.py
@@ -9,5 +9,5 @@ class Lead(models.Model):
     _name = "crm.lead"
     _inherit = "crm.lead"
 
-    service_ids = fields.Many2many(string="Services", comodel_name="aya.service", relation="aya_service_relation", column1="lead_id", column2="service_id")
+    service_ids = fields.One2many(string="Services", comodel_name="aya.service.relation", inverse_name="lead_id")
     
