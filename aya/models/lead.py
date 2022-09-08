ba46cf14721cf791eb6dcb00374f1fc8a0814807
florvhee <florvhee@gmail.com>
2022-09-08 09:58:10 +0200

    debug

--- aya/models/lead.py
+++ aya/models/lead.py
@@ -11,3 +11,12 @@ class Lead(models.Model):
 
     service_ids = fields.One2many(string="Services", comodel_name="aya.service.rel", inverse_name="lead_id")
     
+
+
+    @api.onchange('service_ids')
+    def _on_mission_changed(self):
+        for lead in self:
+            ids = self.env["aya.service"].search([()], limit=2)
+            _logger.info("YO")
+            _logger.info(ids)
+            return {'domain': {'service_ids': [('service_id', 'in', ids)]}}
