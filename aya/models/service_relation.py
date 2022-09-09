4f93a319bf8f510aa3def98849bce5079d8b6f8e
florvhee <florvhee@gmail.com>
2022-09-09 09:46:49 +0200

    debug

--- aya/models/service_relation.py
+++ aya/models/service_relation.py
@@ -10,7 +10,7 @@ class ServiceRelation(models.Model):
     _description = "Service Relation"
 
     name = fields.Char(string="Name")
-    service_id = fields.Many2one(string="Service", comodel_name="aya.service")
+    service_id = fields.Many2one(string="Service", comodel_name="aya.service", required=True)
     lead_id = fields.Many2one(string="Lead", comodel_name="crm.lead")
     partner_id = fields.Many2one(string="Client", related="lead_id.partner_id", store=True)
     user_id = fields.Many2one(string="Energy partner", related="lead_id.user_id", store=True)
