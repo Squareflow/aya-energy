a1ec9755438abe6492d98c53f8066b099a3510f9
florvhee <florvhee@gmail.com>
2022-09-28 00:41:14 +0200

    add enrichment

--- aya/models/sale_order.py
+++ aya/models/sale_order.py
@@ -1,7 +1,7 @@
 from odoo import _, api, fields, models
 
 import logging
-
+import datetime
 
 _logger = logging.getLogger(__name__)
 
@@ -29,10 +29,13 @@ class SaleOrder(models.Model):
         for order in self:
             order.state = "approved"
             if order.partner_id:
-                order.message_post(partner_ids=[order.user_id.partner_id.id], body="The offer "+order.name+" for the client "+order.partner_id.name+" has been approved. You can now send it.")
+                order.activity_schedule("note.mail_activity_data_reminder", summary='Offer approved', note= "The offer "+order.name+" for the client "+order.partner_id.name+" has been approved. You can now send it.", user_id= order.user_id.id, date_deadline= datetime.datetime.now()+ datetime.timedelta(days=1))
+                
+                #order.message_post(partner_ids=[order.user_id.partner_id.id], body="The offer "+order.name+" for the client "+order.partner_id.name+" has been approved. You can now send it.")
 
     def reject_order(self):
         for order in self:
             order.state = "draft"
             if order.partner_id:
-                order.message_post(partner_ids=[order.user_id.partner_id.id], body="The offer "+order.name+" for the client "+order.partner_id.name+" has been rejected please modify it.")
+                order.activity_schedule("note.mail_activity_data_reminder", summary='Offer rejected', note= "The offer "+order.name+" for the client "+order.partner_id.name+" has been rejected please modify it.", user_id= order.user_id.id, date_deadline= datetime.datetime.now()+ datetime.timedelta(days=1))
+               #order.message_post(partner_ids=[order.user_id.partner_id.id], body="The offer "+order.name+" for the client "+order.partner_id.name+" has been rejected please modify it.")
