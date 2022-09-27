f174f60685815fa6a845e89b58cd6fd2e53aa7a9
florvhee <florvhee@gmail.com>
2022-09-27 22:53:37 +0200

    add enrichment

--- aya/models/sale_order_line.py
+++ aya/models/sale_order_line.py
@@ -30,3 +30,41 @@ class SaleOrderLine(models.Model):
         for line in self:
             if line.order_id and line.order_id.partner_id:
                 return {'domain': {'site_ids': [('type','=', 'site'),('id', 'in', line.order_id.partner_id.child_ids.ids)]}}
+
+    def write(self, vals):
+        old_categ = self.categ_id
+        res = super(SaleOrderLine, self).write(vals)
+        new_categ = self.categ_id
+        if old_categ != new_categ:
+            self.handle_unlink_rel(old_categ)
+            self.handle_create_rel(new_categ)
+
+        return res
+
+    def handle_create_rel(self, categ):
+        for line in self:
+            if line.product_id and line.order_id.opportunity_id:
+                rel = self.env["aya.service.rel"].search([("lead_id", "=", line.order_id.opportunity_id.id), ("service_id","=", categ.id)], limit=1)
+                if not rel:
+                    rel = self.env["aya.service.rel"].create({
+                        "lead_id": line.order_id.opportunity_id.id,
+                        "service_id": categ.id,
+                    })            
+        
+    @api.model
+    def create(self, vals):
+        obj = super(SaleOrderLine, self).create(vals)
+        obj.handle_create_rel(obj.categ_id)
+        return obj
+
+    def handle_unlink_rel(self, categ):
+        for line in self:
+            if line.order_id.opportunity_id and line.product_id:
+                rel = self.env["aya.service.rel"].search([("lead_id", "=", line.order_id.opportunity_id.id), ("service_id","=", categ.id)], limit=1)
+                if rel:
+                    rel.unlink()            
+
+    def unlink(self):
+        for line in self:
+            line.handle_unlink_rel(line.categ_id)
+        return super(SaleOrderLine, self).unlink()
