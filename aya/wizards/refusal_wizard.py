c671de839c09a46d5bbb7472d97c5f7004a42dd5
florvhee <florvhee@gmail.com>
2022-09-28 19:33:44 +0200

    add refusal popup

--- aya/wizards/refusal_wizard.py
+++ aya/wizards/refusal_wizard.py
@@ -17,5 +17,5 @@ class RefusalWizard(models.TransientModel):
         for wizard in self:
             wizard.order_id.state = "draft"
             if wizard.order_id.partner_id:
-                wizard.order_id.activity_schedule("note.mail_activity_data_reminder", summary='Offer rejected', note= "The offer "+wizard.order_id.name+" for the client "+wizard.order_id.partner_id.name+" has been rejected please modify it.\n"+wizard.refusal_comment, user_id= wizard.order_id.user_id.id, date_deadline= datetime.datetime.now()+ datetime.timedelta(days=1))
+                wizard.order_id.activity_schedule("note.mail_activity_data_reminder", summary='Offer rejected', note= "The offer "+wizard.order_id.name+" for the client "+wizard.order_id.partner_id.name+" has been rejected please modify it.\n <br/> Refusal reason:"+wizard.refusal_comment, user_id= wizard.order_id.user_id.id, date_deadline= datetime.datetime.now()+ datetime.timedelta(days=1))
             #wizard.order_id.message_post(body=wizard.refusal_comment)
